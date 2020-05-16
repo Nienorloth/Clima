6035af3bc6c53ff1dac2e03005077d1b0fd55afd
Mariana Rascon <nienorloth@gmail.com>
2020-05-16 17:35:10 -0500

    adding all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
 
 function App() {
+
+    //state del formulario
+    const [ busqueda, guardarBusqueda ] = useState({
+      ciudad: '',
+      pais: ''
+  });
+
+    const [ consultar, guardarConsultar ] = useState(false);
+  const { ciudad, pais } = busqueda;
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      if (consultar) {
+      const appId = 'c5b285e1c97ebe7588259b50e948f8ac';
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      console.log(resultado);  
+      }    
+    }
+    consultarAPI();
+  },[consultar]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header 
+        titulo = 'Clima React App'
+      /> 
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario 
+                busqueda={busqueda}
+                guardarBusqueda={guardarBusqueda}
+                guardarConsultar={guardarConsultar}
+              /> 
+            </div>
+            <div className="col m6 s12">
+             2
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
